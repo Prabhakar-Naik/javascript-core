9fce6a2f3554bf0d61e9ba7704df60df74ca4739
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-14 18:20:13 +0530

    added

--- core/arthematicOperators.js
+++ core/arthematicOperators.js
@@ -3,3 +3,15 @@ var num2 = 34;
 
 var add = num1 + num2;
 console.log(add);
+
+var substract = num2 - num1;
+console.log(substract);
+
+var muliply = num1 * num2;
+console.log(muliply);
+
+var devision = num1 / num2;
+console.log(devision);
+
+var remainder = num1 % num2;
+console.log(remainder);
