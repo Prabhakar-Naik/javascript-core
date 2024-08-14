ab80c05b21760cc8dfceacfb5a10cafd02171320
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-14 18:19:27 +0530

    changes

---
+++ core/variablesTypes.js
@@ -0,0 +1,27 @@
+console.log("Hello world");
+
+// alert("Hi I am alert working on web only");
+
+var number = 23;
+var name = "prabha";
+var counties = ["India", "Japan", "Russia", "USA", "Britan"];
+var numbers = [1, 3, 25, 35, 78, 5, 2, 98];
+var characher = "S";
+var boolean = true;
+var myVar = {
+  name: "prabha",
+  age: 25,
+  qualities: ["sleep", "coding"],
+  description: {
+    size: "180cm",
+    weight: 65,
+  },
+};
+
+console.log(number + " " + typeof number);
+console.log(name, typeof name);
+console.log(counties + " " + typeof counties);
+console.log(numbers + " " + typeof numbers);
+console.log(characher + " " + typeof characher);
+console.log(myVar + " " + typeof myVar);
+console.log(boolean + " " + typeof boolean);
--- tutor/arthematicOperators.js
+++
@@ -1,5 +0,0 @@
-var num1 = 20;
-var num2 = 34;
-
-var add = num1 + num2;
-console.log(add);
