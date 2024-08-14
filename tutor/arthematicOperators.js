4feeb7a0044d2c76f7095b0e95c422f3900d1b08
Prabhakar Naik <prabhakarnaik225@gmail.com>
2024-08-14 17:44:31 +0530

    Add files via upload

---
+++ tutor/arthematicOperators.js
@@ -0,0 +1,5 @@
+var num1 = 20;
+var num2 = 34;
+
+var add = num1 + num2;
+console.log(add);
---
+++ tutor/script.js
@@ -0,0 +1,37 @@
+console.log("Hi prabha");
+// alert("Hello");
+var myVar = "prabha";
+console.log(myVar);
+
+myVar = "12";
+console.log(myVar);
+
+myVar = 12;
+console.log(myVar);
+
+myVar = ["hello", "bye", 2024, "coming", 2025];
+console.log(myVar);
+
+myVar = {};
+console.log(myVar);
+
+myVar = {
+  name: "veera",
+  age: 30,
+  qualities: ["cigarate", "coding"],
+  description: {
+    size: "180cm",
+    weight: 60,
+  },
+};
+
+console.log(myVar);
+
+myVar = true;
+console.log(myVar);
+
+myVar = false;
+console.log(myVar);
+
+myVar = 1;
+console.log(myVar);
